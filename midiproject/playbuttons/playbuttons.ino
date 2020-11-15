c805fb533b5c5943ae4d0f349bc52fa9188390d9
xfillmore <xuedan.fillmore@colorado.edu>
2020-11-14 19:45:16 -0700

    changed led var to buzzer and reorganized commented coded

--- midiproject/playbuttons/playbuttons.ino
+++ midiproject/playbuttons/playbuttons.ino
@@ -1,15 +1,17 @@
 #include <pitches.h>
 
-//const int currentButton = 8;
-//const int buttons = [2, 3, 4, 5, 6, 7, 8];
-const int led = 13;
-//int currentButtonState = 0;
-//int buttonStates = [0, 0, 0, 0, 0, 0, 0];
+const int buzzer = 13;
+
+//  const int currentButton = 8;
+//  int currentButtonState = 0;
+
+//  const int buttons = [2, 3, 4, 5, 6, 7, 8];
+//  int buttonStates = [0, 0, 0, 0, 0, 0, 0];
 
 void setup() {
 
-//  pinMode(buttons, INPUT_PULLUP);
-//  pinMode(currentButton, INPUT_PULLUP);
+  //  pinMode(buttons, INPUT_PULLUP);
+  //  pinMode(currentButton, INPUT_PULLUP);
 
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
@@ -19,37 +21,38 @@ void setup() {
   pinMode(7, INPUT_PULLUP);
   pinMode(8, INPUT_PULLUP);
 
-  pinMode(led, OUTPUT);
-
+  pinMode(buzzer, OUTPUT);
 
   Serial.begin(9600);
-
-
 }
 
 void loop() {
-//  currentButtonState = digitalRead(currentButton);
-//  Serial.println(currentButtonState);
-  
-//    for (int i=0; i<sizeof(buttons); i++) {
-//      buttonStates[i] = digitalRead(buttons[i]);
-//    }
-//  
-//    for (int i=0; i<sizeof(buttons); i++) {
-//      if (buttonStates[i] == HIGH) {
-//        digitalWrite(led, HIGH);
-//      }
-//      else {
-//        digitalWrite(led, LOW);
-//      }
-//    }
+  //  currentButtonState = digitalRead(currentButton);
+  //  Serial.println(currentButtonState);
+  //
+  //  if (currentButtonState == HIGH) {
+  //    digitalWrite(buzzer, LOW);
+  //  }
+  //  else {
+  //    digitalWrite(buzzer, HIGH);
+  //  }
 
-//  if (currentButtonState == HIGH) {
-//    digitalWrite(led, LOW);
-//  }
-//  else {
-//    digitalWrite(led, HIGH);
-//  }
+
+
+  //  for (int i=0; i<sizeof(buttons); i++) {
+  //    buttonStates[i] = digitalRead(buttons[i]);
+  //  }
+  //
+  //  for (int i=0; i<sizeof(buttons); i++) {
+  //    if (buttonStates[i] == HIGH) {
+  //      digitalWrite(buzzer, HIGH);
+  //    }
+  //    else {
+  //      digitalWrite(buzzer, LOW);
+  //    }
+  //  }
+
+
 
   int key1 = digitalRead(2);
   int key2 = digitalRead(3);
@@ -59,26 +62,54 @@ void loop() {
   int key6 = digitalRead(7);
   int key7 = digitalRead(8);
 
-  if (key1 == LOW) { tone(led, NOTE_C4); }
-  else { noTone(led); }
-  
-  if (key2 == LOW) { tone(led, NOTE_D4); }
-  else { noTone(led); }
-  
-  if (key3 == LOW) { tone(led, NOTE_E4); }
-  else { noTone(led); }
-  
-  if (key4 == LOW) { tone(led, NOTE_F4); }
-  else { noTone(led); }
-  
-  if (key5 == LOW) { tone(led, NOTE_G4); }
-  else { noTone(led); }
-  
-  if (key6 == LOW) { tone(led, NOTE_A4); }
-  else { noTone(led); }
-  
-  if (key7 == LOW) { tone(led, NOTE_B4); }
-  else { noTone(led); }
+  if (key1 == LOW) {
+    tone(buzzer, NOTE_C4);
+  }
+  else {
+    noTone(buzzer);
+  }
+
+  if (key2 == LOW) {
+    tone(buzzer, NOTE_D4);
+  }
+  else {
+    noTone(buzzer);
+  }
+
+  if (key3 == LOW) {
+    tone(buzzer, NOTE_E4);
+  }
+  else {
+    noTone(buzzer);
+  }
+
+  if (key4 == LOW) {
+    tone(buzzer, NOTE_F4);
+  }
+  else {
+    noTone(buzzer);
+  }
+
+  if (key5 == LOW) {
+    tone(buzzer, NOTE_G4);
+  }
+  else {
+    noTone(buzzer);
+  }
+
+  if (key6 == LOW) {
+    tone(buzzer, NOTE_A4);
+  }
+  else {
+    noTone(buzzer);
+  }
+
+  if (key7 == LOW) {
+    tone(buzzer, NOTE_B4);
+  }
+  else {
+    noTone(buzzer);
+  }
 
 
 
